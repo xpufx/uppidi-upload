bd5ce37b11a44eace1844cccd60b437e2099e880
xpufx <github@xpufx.com>
2026-05-09 07:08:51 +0000

    chore: bump version to 1.1.0+1

--- lib/core/version.dart
+++ lib/core/version.dart
@@ -1,3 +1,4 @@
-const String gitHash = String.fromEnvironment('GIT_HASH', defaultValue: 'unknown');
+const String gitHash =
+    String.fromEnvironment('GIT_HASH', defaultValue: 'unknown');
 const String cdnUrl = String.fromEnvironment('CDN_URL', defaultValue: '');
-const String appVersion = '1.0.0+1';
+const String appVersion = '1.1.0+1';
