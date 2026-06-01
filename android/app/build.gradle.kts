8c3f69017fc52476669e6335bc59fad0091f8f6c
xpufx <github@xpufx.com>
2026-06-01 22:54:14 +0000

    feat: APK signing with release keystore + CI setup
    - Generated upload-keystore.jks (SHA256withRSA, 2048-bit, 10000 days)
    - android/key.properties with store/key passwords (gitignored)
    - build.gradle.kts reads key.properties for release signing config
    - .gitignore excludes *.keystore, *.jks, key.properties
    - GitHub CI decodes KEYSTORE_BASE64 secret and creates key.properties
    - CI secrets needed: KEYSTORE_BASE64, KEYSTORE_PASSWORD, KEY_PASSWORD, KEY_ALIAS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,59 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.FileInputStream
+
+android {
+    namespace = "com.uppidi.uppidi"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.uppidi.uppidi"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            val keystorePropsFile = rootProject.file("key.properties")
+            if (keystorePropsFile.exists()) {
+                val props = Properties()
+                props.load(FileInputStream(keystorePropsFile))
+                storeFile = rootProject.file(props.getProperty("storeFile"))
+                storePassword = props.getProperty("storePassword")
+                keyAlias = props.getProperty("keyAlias")
+                keyPassword = props.getProperty("keyPassword")
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.findByName("release") ?: signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
 android {
     namespace = "com.uppidi.uppidi"
     compileSdk = flutter.compileSdkVersion
